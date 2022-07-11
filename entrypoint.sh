8e623da467449a9e79b65a4fe43147623158b9ff
Thorsten Ludewig <t.ludewig@gmail.com>
2022-07-11 11:26:37 +0200

    remove duplicate code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
   echo '*** Initialize /etc/ssh directory'
-  mkdir -p /etc/ssh /var/log
-  touch /var/log/wtmp
+  mkdir -p /etc/ssh
   cp -a /etc/ssh.orig/* /etc/ssh/
   ssh-keygen -A
 fi
